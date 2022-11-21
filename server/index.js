69d778f81edabe928fd901a09a6df3d595b9b5f5
Rhoda P <rhodaphillipsosei@gmail.com>
2022-11-21 07:32:20 -0500

    set up mongoose cluster

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://rhoda123:rhoda123@cluster0.2umfpw6.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//   mongoose.set('useFindAndModify', false);
