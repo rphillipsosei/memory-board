c0fa88ff09b3401ae5ded806487a5841a7c0c433
Rhoda P <rhodaphillipsosei@gmail.com>
2022-11-22 18:23:24 -0500

    create controller folder

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js'
+
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-res.send("This works")
-})
+
+router.get('/', getPosts)
 
 export default router;
